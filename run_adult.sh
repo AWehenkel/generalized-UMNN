2ac74b119e23b77510a63159426e4bcb97b1f694
awehenkel <antoine.wehenkel@gmail.com>
2019-12-14 22:39:46 +0100

    Update for gpu

--- run_adult.sh
+++ run_adult.sh
@@ -9,15 +9,15 @@
 #SBATCH --export=ALL
 #SBATCH --parsable
 #SBATCH --job-name "ADULT_MONOTONICITY"
-#SBATCH --output "logging/ADULT_MONOTONICITY_%a.log"
+#SBATCH --output "ADULT_MONOTONICITY_%a.log"
 #SBATCH --requeue
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4000
 #SBATCH --ntasks=1
 #SBATCH --time="2-24:00:00"
 #SBATCH --gres=gpu:1
-#3
+#
 
 cd experiments/adult
 conda activate UMNN
-srun python AdultExperiment.py
+python AdultExperiment.py
