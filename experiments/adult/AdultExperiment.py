88649d19250accd490b3a395416674d571345a20
awehenkel <antoine.wehenkel@gmail.com>
2019-12-14 23:48:57 +0100

    Update for gpu

--- experiments/adult/AdultExperiment.py
+++ experiments/adult/AdultExperiment.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from Dataloader import AdultDataset
-from ...models import SlowDMonotonicNN
+from models import SlowDMonotonicNN
 from tensorboardX import SummaryWriter
 
 
 writer = SummaryWriter()
-train_ds = AdultDataset("../../data/adult/adult.data")
-test_ds = AdultDataset("../../data/adult/adult.test", test=True)
+train_ds = AdultDataset("data/adult/adult.data")
+test_ds = AdultDataset("data/adult/adult.test", test=True)
 
 train_dl = DataLoader(train_ds, 100, shuffle=True, num_workers=4)
 test_dl = DataLoader(test_ds, 100, shuffle=True, num_workers=4)
@@ -18,7 +18,7 @@ x, y = train_ds[1]
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 #net = nn.Sequential(nn.Linear(len(x), 200), nn.ReLU(), nn.Linear(200, 200), nn.ReLU(), nn.Linear(200, 1))#
 net = SlowDMonotonicNN(4, len(x) - 4, [150, 150, 150, 150], 1, device)
-net.load_state_dict(torch.load("model.ckpt"))
+#net.load_state_dict(torch.load("model.ckpt"))
 optim = Adam(net.parameters(), lr=.001)
 loss_f = nn.BCELoss()
 sigmoid = nn.Sigmoid()
