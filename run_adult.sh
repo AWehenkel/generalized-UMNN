35f01ebeb55e68e2e10cb3f48496f99e40b7685b
awehenkel <antoine.wehenkel@gmail.com>
2019-12-14 22:35:06 +0100

    Update for gpu

---
+++ run_adult.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# This file:
+#
+#  -
+#
+# Slurm arguments.
+#
+#SBATCH --export=ALL
+#SBATCH --parsable
+#SBATCH --job-name "ADULT_MONOTONICITY"
+#SBATCH --output "logging/ADULT_MONOTONICITY_%a.log"
+#SBATCH --requeue
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4000
+#SBATCH --ntasks=1
+#SBATCH --time="2-24:00:00"
+#SBATCH --gres=gpu:1
+#3
+
+cd experiments/adult
+python AdultExperiment.py
