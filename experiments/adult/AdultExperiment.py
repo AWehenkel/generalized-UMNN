681081a3b6ba60a63a5f984cf203fa227085a1e9
awehenkel <antoine.wehenkel@gmail.com>
2019-12-15 02:14:40 +0100

    Update for gpu

--- experiments/adult/AdultExperiment.py
+++ experiments/adult/AdultExperiment.py
@@ -19,9 +19,28 @@ def run_adult_experiment():
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     embedding_net = nn.Sequential(nn.Linear(len(x) - 4, 200), nn.ReLU(),
                                   nn.Linear(200, 200), nn.ReLU(),
-                                  nn.Linear(200, 20), nn.ReLU()).to(device)
-    net = SlowDMonotonicNN(4, 20, [30, 30, 30], 1, 100, device)
-    #net.load_state_dict(torch.load("model.ckpt"))
+                                  nn.Linear(200, 100), nn.ReLU()).to(device)
+    net = SlowDMonotonicNN(4, 100, [100, 100, 100], 1, 300, device)
+    if False:
+        net.load_state_dict(torch.load("model.ckpt"))
+        x = torch.randn(500, 4)
+        h = torch.zeros(500, 100)
+        with torch.no_grad():
+            import matplotlib.pyplot as plt
+            plt.subplot(221)
+            y = net(torch.cat((x[:, [0]], torch.zeros(500, 3)), 1), h)
+            plt.scatter(x[:, 0], y)
+            plt.subplot(222)
+            y = net(torch.cat((torch.zeros(500, 1), x[:, [1]], torch.zeros(500, 2)), 1), h)
+            plt.scatter(x[:, 1], y)
+            plt.subplot(223)
+            y = net(torch.cat((torch.zeros(500, 2), x[:, [2]], torch.zeros(500, 1)), 1), h)
+            plt.scatter(x[:, 2], y)
+            plt.subplot(224)
+            y = net(torch.cat((torch.zeros(500, 3), x[:, [3]]), 1), h)
+            plt.scatter(x[:, 3], y)
+        plt.show()
+        exit()
     optim = Adam(net.parameters(), lr=.001, weight_decay=1e-5)
     loss_f = nn.BCELoss()
     sigmoid = nn.Sigmoid()
@@ -31,9 +50,9 @@ def run_adult_experiment():
         i = 0
         avg_accuracy = 0.
         for x, y in train_dl:
-            x,y = x.float().to(device), y.float().to(device)
+            x,y = x.float().to(device), y.unsqueeze(1).float().to(device)
             h = embedding_net(x[:, 4:])
-            y_est = sigmoid(net(x[:, :4], h)).squeeze(1)
+            y_est = sigmoid(net(x[:, :4], h))
             loss = loss_f(y_est, y)
             optim.zero_grad()
             loss.backward()
@@ -53,9 +72,9 @@ def run_adult_experiment():
         net.set_steps(100)
         for x, y in test_dl:
             with torch.no_grad():
-                x, y = x.float().to(device), y.float().to(device)
+                x, y = x.float().to(device), y.unsqueeze(1).float().to(device)
                 h = embedding_net(x[:, 4:])
-                y_est = sigmoid(net(x[:, :4], h)).squeeze(1)
+                y_est = sigmoid(net(x[:, :4], h))
                 loss = loss_f(y_est, y)
                 avg_loss += loss.item()
                 avg_accuracy += torch.abs((y_est > .5).float() == y).float().mean()
